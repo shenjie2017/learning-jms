001270c47b1ee40d452a7d9e5b8c8b4667a4ce2c
shenjie <1075850619@qq.com>
2017-12-28 17:58:37 +0800

    测试能否使用

---
+++ MQ-activeMQ-demo/src/main/java/com/blue/topic/boot/A.java
@@ -0,0 +1,29 @@
+package com.blue.topic.boot;
+
+/**
+ * @author shenjie
+ * @version v1.0
+ * @Description
+ * @Date: Create in 17:57 2017/12/28
+ * @Modifide By:
+ **/
+
+//      ┏┛ ┻━━━━━┛ ┻┓
+//      ┃　　　　　　 ┃
+//      ┃　　　━　　　┃
+//      ┃　┳┛　  ┗┳　┃
+//      ┃　　　　　　 ┃
+//      ┃　　　┻　　　┃
+//      ┃　　　　　　 ┃
+//      ┗━┓　　　┏━━━┛
+//        ┃　　　┃   神兽保佑
+//        ┃　　　┃   代码无BUG！
+//        ┃　　　┗━━━━━━━━━┓
+//        ┃　　　　　　　    ┣┓
+//        ┃　　　　         ┏┛
+//        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+//          ┃ ┫ ┫   ┃ ┫ ┫
+//          ┗━┻━┛   ┗━┻━┛
+
+public class A {
+}
